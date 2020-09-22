0c1ee8722e2625854c8c6fc4f0c1f7d1f3ec3f4b
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2020-09-22 11:02:13 +0300

    change package for phone mask add phone input validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import IMask from 'imask';
+import Inputmask from "inputmask";
 
 class Form extends React.Component {
     constructor() {
@@ -27,22 +27,31 @@ class Form extends React.Component {
     render() {
         window.addEventListener('DOMContentLoaded', () => {
             const phoneInput = document.getElementById('phone-input');
-
-            IMask(phoneInput,
-                {
-                    mask: '+{7} (000) 000-00-00'
-                });
+            let im = new Inputmask("+7 (999) 999-99-99");
+            im.mask(phoneInput);
         });
 
         return (
             <form className='form' id='form' onChange={this.handleChange}
                   onSubmit={(e) => {
                       e.preventDefault();
-                      this.props.saveNewPosting(this.state.title, this.state.content,
-                          this.state.phone, this.state.city);
+                      let phoneInput = document.getElementById('phone-input');
+                      if (phoneInput.value.includes('_')) {
+                          const error = document.createElement('span');
+                          error.className = 'form__error';
+                          error.innerText = 'Поле заполнено неверно!';
+                          phoneInput.before(error);
+                      } else {
+                          if (document.getElementsByClassName('form__error')[0]) {
+                              document.getElementsByClassName('form__error')[0].remove();
+                          }
+                          this.props.saveNewPosting(this.state.title, this.state.content,
+                              this.state.phone, this.state.city);
+                      }
                   }}>
                 <h2 className='form__title'>Добавьте новое объявление:</h2>
-                <label className='form__label form__label_req' htmlFor='title-input'>Заголовок: (макс. 140 символов)</label>
+                <label className='form__label form__label_req' htmlFor='title-input'>Заголовок: (макс. 140
+                    символов)</label>
                 <input className='form__input' id='title-input' type='text' maxLength='140' required/>
                 <label className='form__label' htmlFor='content-input'>Текст объявления: (макс. 300 символов)</label>
                 <textarea className='form__textarea' id='content-input' maxLength='300'/>
@@ -51,7 +60,7 @@ class Form extends React.Component {
                        required/>
                 <label className='form__label' htmlFor='city-select'>Город:</label>
                 <select className='form__select' id='city-select' defaultValue=''>
-                    <option className='form__city-option' disabled> </option>
+                    <option className='form__city-option' disabled></option>
                     <option className='form__city-option'>Москва</option>
                     <option className='form__city-option'>Санкт-Петербург</option>
                     <option className='form__city-option'>Казань</option>
